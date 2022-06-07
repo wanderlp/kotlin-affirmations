04bf975cbac8cbe83f68b538afcb59902e04b1e3
Wanderson <lopez.wanderson@gmail.com>
2022-06-07 15:33:24 -0600

    Implementación del ItemAdapter

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.affirmations.adapter
 
-class ItemAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // El administrador de diseño llama este método para crear nuevas interfaces de vista
+        // para RecyclerView (cuando no hay contenedores de vistas existentes que puedan reutilizarse)
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        // El administrador de diseño llama a este método para reemplazar el contenido de una vista
+        // de elementos de lista
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
 }
